bee8678ec8a86b53979c77f9b56d6cf1b4567893
Rick Dias <rickelmegadias@gmail.com>
2021-08-25 00:36:38 -0300

    modified crop system working

--- front-end/src/dog_cards/create_dog_forms.js
+++ front-end/src/dog_cards/create_dog_forms.js
@@ -22,48 +22,44 @@ function sendPostNewDog(e) {
 
 let imagePreview = document.querySelector('img#picture_preview');
 
-function updateImageChange(e) {
-    e.preventDefault();
-    
-    const [File] = e.target.files;
+let SliderElementX = document.getElementById('slider_x');
+let SliderElementY = document.getElementById('slider_y');
+let SliderElementZoom = document.getElementById('slider_zoom');
 
-    if (File) {
-        imagePreview.src = URL.createObjectURL(File);
-    }
+let cutterPreview = document.getElementById('cutter_container');
+
+let imageHeight = 0;
+let imageWidth = 0;
+
+imagePreview.onload = function(){
+    imageHeight = imagePreview.height;
+    imageWidth = imagePreview.width;
+    console.log(imageHeight, imageWidth);
+    // code here to use the dimensions
+    
+    SliderElementX.min = 0;
+    SliderElementX.max = imageWidth-300;
+    SliderElementX.value = 0;
+
+    SliderElementY.min = 0;
+    SliderElementY.max = imageHeight-300;
+    SliderElementY.value = 0;
+
+    // SliderElementZoom.min = 0;
+    // SliderElementZoom.max = imageHeight-300;
+    // SliderElementZoom.value = 0;
 }
 
-
-/*
- *
- *  CUT IMAGE:
- * 
-*/
-const cutterContaier = document.querySelector('div#cutter_container');
-const picture_preview = document.querySelector('img#picture_preview');
-
-let oldPos;
 let x = 0; let y = 0;
-function mouseMove (e) {
 
-    const { offsetX: off_x, offsetY: off_y, pageX: px, pageY: py } = e;
-    const a = picture_preview.getBoundingClientRect();
-
-    if ( oldPosX < e.pageX ){
-        x++;
-    }else{
-        x--;
-    }
-
-    if ( oldPosY < e.pageY ){
-        // picture_preview.style.left = `${x}px`;
-        // console.log('dir')
-        y++;
-    }else{
-        // picture_preview.style.left = `${-x}px`;
-        // console.log('esq');
-        y--;
-    }
-    
+SliderElementX.oninput = function() {
+    x = -(SliderElementX.value)+(imageWidth/2)-(150);
+    console.log(x);
+    defineTransform(x, y);
+}
+SliderElementY.oninput = function() {
+    y = -(SliderElementY.value)+(imageHeight/2)-(150);
+    console.log(y);
     defineTransform(x, y);
 }
 
@@ -71,48 +67,122 @@ function defineTransform(x, y) {
     picture_preview.style.transform = `translate(${x}px,${y}px)`;
 }
 
-function mouseScroll(e){
-    console.log(picture_preview.scrollTop);
-    console.log(e.scrollTop);
-    // picture_preview.style.transform = `scale(${x}px,${y}px)`;
+
+function updateImageChange(e) {
+    e.preventDefault();
+    
+    const [File] = e.target.files;
+    
+    if (File) {
+        imagePreview.src = URL.createObjectURL(File);
+    }
 }
 
 
-cutterContaier.onmouseover = function () {
-    // console.log('On mouse hover!');
-    cutterContaier.addEventListener('scroll', mouseScroll);
-}
 
 
-cutterContaier.onmousedown = function (e) {
-    oldPosX = e.pageX;
-    oldPosY = e.pageY;
-    cutterContaier.addEventListener('mousemove', mouseMove);
-}
 
-cutterContaier.onmouseup = function () {
-    cutterContaier.removeEventListener('mousemove', mouseMove);
-    // console.log('Mouse Up !');
-}
 
-cutterContaier.onmouseleave = function () {
-    // picture_preview.style.transform = `translate(${0}px,${0}px)`;
-    // console.log(x,y);
-    cutterContaier.removeEventListener('mousemove', mouseMove);
-        // console.log('Mouse Leave!');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
- *
- *  SLIDER IMAGE:
- * 
+*   ===================================
+*   CUT IMAGE USING MOUSE HALF WORKING:
+*   ===================================
 */
+/*
+ let oldPos;
+ let oldPositionSlider = SliderElementX.value;
+
+ const cutterContaier = document.querySelector('div#cutter_container');
+ const picture_preview = document.querySelector('img#picture_preview');
+
+ function mouseMove (e) {
+     const { offsetX: off_x, offsetY: off_y, pageX: px, pageY: py } = e;
+     const a = picture_preview.getBoundingClientRect();
+
+     if ( oldPosX < e.pageX ){
+         x++;
+     }else{
+         x--;
+     }
+
+     if ( oldPosY < e.pageY ){
+         y++;
+     }else{
+         y--;
+     }
+    
+     defineTransform(x, y);
+ }
+
+ function mouseScroll(e){
+     console.log(picture_preview.scrollTop);
+     console.log(e.scrollTop);
+     // picture_preview.style.transform = `scale(${x}px,${y}px)`;
+ }
 
 
-// function imageSlider_X () {
-//     const image_infos = cutterContaier.getBoundingClientRect();
-//     console.log(image_infos);
-//     console.log(document.querySelector("#picture_preview").naturalHeight);
-// }
+ cutterContaier.onmouseover = function () {
+     // console.log('On mouse hover!');
+     cutterContaier.addEventListener('scroll', mouseScroll);
+ }
 
-// imageSlider_X();
+
+ cutterContaier.onmousedown = function (e) {
+     oldPosX = e.pageX;
+     oldPosY = e.pageY;
+     cutterContaier.addEventListener('mousemove', mouseMove);
+ }
+
+ cutterContaier.onmouseup = function () {
+     cutterContaier.removeEventListener('mousemove', mouseMove);
+     // console.log('Mouse Up !');
+ }
+
+ cutterContaier.onmouseleave = function () {
+     // picture_preview.style.transform = `translate(${0}px,${0}px)`;
+     // console.log(x,y);
+     cutterContaier.removeEventListener('mousemove', mouseMove);
+         // console.log('Mouse Leave!');
+ }
+
+ function defineTransform(x, y) {
+     picture_preview.style.transform = `translate(${x}px,${y}px)`;
+ }
+
+
+
+  SLIDER IMAGE:
+ 
+
+
+
+ function imageSlider_X () {
+     const image_infos = cutterContaier.getBoundingClientRect();
+     console.log(image_infos);
+     console.log(document.querySelector("#picture_preview").naturalHeight);
+ }
+
+ imageSlider_X();
+
+*/
