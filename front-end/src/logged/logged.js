c83c0f4d3bbbbb54779ac0d45cd5d5483a6ecb8d
Rick Dias <rickelmegadias@gmail.com>
2021-09-08 12:28:04 -0300

    Login front-end finished

--- front-end/src/logged/logged.js
+++ front-end/src/logged/logged.js
@@ -1,7 +1,19 @@
-function CreatePageUserLogged() {
-        getMyLoginInfo();
+async function CreatePageUserLogged() {
+        const LoginInfo = await getMyLoginInfo();
         const pageLogged = document.querySelector('main');
-        pageLogged.innerHTML = "<div>Teste</div>";
+        pageLogged.innerHTML = `
+        <section class="modal-log-reg">
+
+        <article>
+                <label for="">Dados da Conta</label>
+                <label for="" class="label-h1">Meu Nome de Login: ${LoginInfo.user}</label>
+                <label for="" class="label-h1">Meu Nome: ${LoginInfo.user_name}</label>
+                <label for="" class="label-h1">Meus Posts: ${LoginInfo.dogs_posted} cachorros</label>
+                <p><a onclick="exitAccount()">Sair da Conta.</a></p>
+        </article>
+
+        </section>
+        `;
 }
 
 async function getMyLoginInfo() {
@@ -17,6 +29,14 @@ async function getMyLoginInfo() {
         }).then( (response) => {
                 return response.json();
         }).then( (json) => {
-                console.log(json);
+                // console.log(json);
+                return json;
         });
+
+        return consumeApi;
+}
+
+function exitAccount() {
+    localStorage.clear();
+    location.reload();    
 }
